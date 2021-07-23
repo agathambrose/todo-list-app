82111fe8d8c0099d644f6d4d7bb604621a635f46
Agatha Ambrose <73320658+agathambrose@users.noreply.github.com>
2021-07-23 11:32:28 +0100

    Added a toggle for the task done thumb

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,7 +1,9 @@
-import React from "react";
-import { FaThumbsUp, FaTrash } from "react-icons/fa";
+import React, {useState} from "react";
+import { FaThumbsUp, FaThumbsDown, FaTrash } from "react-icons/fa";
 
 const Todo = ({ text, todo, todos, setTodos }) => {
+  const [iconToggle, setIconToggle] = useState(false)
+
   const handleDelete = () => {
     setTodos(todos.filter((item) => item.id !== todo.id));
   };
@@ -18,13 +20,18 @@ const Todo = ({ text, todo, todos, setTodos }) => {
         return item;
       })
     );
+    handleToggle()
   };
+
+  const handleToggle = () => {
+    setIconToggle(!iconToggle)
+  }
 
   return (
     <div className="flex items-center justify-center mx-2 mt-6">
       <li className={`w-64 h-10 pt-2 pl-2 bg-white rounded md:w-96 ${todo.done ? "opacity-25" : ""}`}>{text}</li>
-      <button className="px-3 py-2 mx-4 bg-white rounded" onClick={handleTaskDone}>
-        <FaThumbsUp />
+      <button className={`px-3 py-2 mx-4 ${iconToggle ? "bg-white bg-opacity-25" : "bg-white"} rounded`} onClick={handleTaskDone}>
+        {iconToggle ?  <FaThumbsDown className="text-gray-800 "/> : <FaThumbsUp/>}
       </button>
       <button className="px-3 py-2 bg-white rounded" onClick={handleDelete}>
         <FaTrash />
